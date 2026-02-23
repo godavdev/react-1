3a7989ad18e4baa5be7b91433cdad6238a2801c7
godavdev <dgaduff@gmail.com>
2026-02-22 20:13:01 -0600

    feat: add background color to App component for improved UI

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BetterForm } from './components/better-form'
 function App() {
 
   return (
-    <div className='flex flex-col items-center justify-center h-screen'>
+    <div className='flex flex-col items-center justify-center h-screen bg-slate-500'>
       <BetterForm/>
     </div>
   )
